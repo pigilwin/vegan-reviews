aee649be4d770336f06e57c3fdb85e36325691c2
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-05-25 17:26:33 +0100

    Routes are now defined within application

--- lib/application.dart
+++ lib/application.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vegan_reviews/reviews/reviews.dart';
 
 class Application extends StatelessWidget {
   // This widget is the root of your application.
@@ -13,7 +14,22 @@ class Application extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        initialRoute: '/',
+        onGenerateRoute: onGenerateRoute,
       ),
     );
+  }
+
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    final WidgetBuilder builder = _getRoutes(settings)[settings.name];
+    return MaterialPageRoute(
+      builder: builder
+    );
+  }
+
+  Map<String, WidgetBuilder> _getRoutes(RouteSettings settings) {
+    return {
+      '/': (BuildContext context) => Home()
+    };
   }
 }
