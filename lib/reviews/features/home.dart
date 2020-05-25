d275881cc0cb20e91d261cae021e3c157c8eda8f
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-05-25 17:45:43 +0100

    Adding header

--- lib/reviews/features/home.dart
+++ lib/reviews/features/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vegan_reviews/reviews/reviews.dart';
 
 class Home extends StatefulWidget {
 
@@ -11,6 +13,18 @@ class _HomeState extends State<Home> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: BlocBuilder<ReviewsBloc, ReviewsState>(
+        builder: (BuildContext context, ReviewsState state) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Header()
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
