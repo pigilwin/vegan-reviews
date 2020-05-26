6962136767eef71e0ec2e6490b0ddb734ff75733
TimRemnant <tremnant@dizions.co.uk>
2020-05-26 15:47:12 +0100

    Adding supplier

--- README.md
+++ README.md
@@ -7,18 +7,6 @@
 - Show latest review
 - Sign in to add new reviews
 
-### Create a new review
-
-- Name of food
-- Description of food
-- Limited time or not
-- Worth it or not
-- Price
-- Location of food supplier
-- Out of 10 slider of stars
-- Emoji picker
-- Picture of food
-
 ### List of all reviews
 
 - List all reviews
--- lib/reviews/components/review_editor.dart
+++ lib/reviews/components/review_editor.dart
@@ -18,7 +18,10 @@ class _ReviewEditorState extends State<ReviewEditor> {
   TextEditingController nameController;
   TextEditingController descriptionController;
   TextEditingController priceController;
+  TextEditingController supplierController;
   int rating;
+  bool worthIt;
+  bool limitedTime;
 
   @override
   void initState() {
@@ -26,7 +29,10 @@ class _ReviewEditorState extends State<ReviewEditor> {
     nameController = TextEditingController(text: widget.review.name);
     descriptionController = TextEditingController(text: widget.review.description);
     priceController = TextEditingController(text: widget.review.price.toString());
+    supplierController = TextEditingController(text: widget.review.supplier);
     rating = widget.review.stars ?? 0;
+    worthIt = widget.review.worthIt;
+    limitedTime = widget.review.limited;
   }
 
   @override
@@ -37,7 +43,10 @@ class _ReviewEditorState extends State<ReviewEditor> {
            _getName(),
            _getDescription(),
            _getPrice(),
-           _getStars()
+           _getStars(),
+           _getWorthIt(),
+           _getLimited(),
+           _getSupplier()
         ],
       ),
     );
@@ -114,6 +123,56 @@ class _ReviewEditorState extends State<ReviewEditor> {
         stars: 10,
         rating: rating,
         size: 30.0,
+      ),
+    );
+  }
+
+  Widget _getWorthIt() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: SwitchListTile(
+        value: worthIt,
+        onChanged: (bool newWorthIt){
+          setState(() {
+            worthIt = newWorthIt;
+          });
+        },
+        title: const Text("Worth It?"),
+        subtitle: const Text("Was the food worth it?"),
+      ),
+    );
+  }
+
+  Widget _getLimited() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: SwitchListTile(
+        value: worthIt,
+        onChanged: (bool newWorthIt){
+          setState(() {
+            worthIt = newWorthIt;
+          });
+        },
+        title: const Text("Limited time?"),
+        subtitle: const Text("Was this a limited time option?"),
+      ),
+    );
+  }
+
+  Widget _getSupplier() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: TextFormField(
+        controller: supplierController,
+        decoration: const InputDecoration(
+          labelText: 'Supplier'
+        ),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return "A supplier must be set";
+          }
+          return null;
+        },
       ),
     );
   }
