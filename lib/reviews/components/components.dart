bd0cf12c9e1226fb7825159d7375720299220f6b
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-05-26 23:32:06 +0100

    Adding seperate card for latest review

--- lib/reviews/components/components.dart
+++ lib/reviews/components/components.dart
@@ -1,5 +1,6 @@
 export './food_type_selector.dart';
 export './header.dart';
+export './latest_review_overview_card.dart';
 export './review_editor.dart';
 export './review_overview_card.dart';
 export './star_rating.dart';
