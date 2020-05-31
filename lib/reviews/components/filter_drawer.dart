0f2d8f1fbba5effe107df6079ccc6638c5ac1323
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-05-31 01:42:33 +0100

    Adding drawer header and background color

--- lib/reviews/components/filter_drawer.dart
+++ lib/reviews/components/filter_drawer.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:vegan_reviews/shared/shared.dart';
 
 class FilterDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: VeganGradient.gradient(0.2)
+        ),
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              child: Text("Filter Reviews", style: TextStyle(fontSize: 22)),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
