be46a87b6ad5f3e191fa7e6e4ec0526eda25c483
Tim Remnant <timremnant@Tims-MacBook-Pro.local>
2020-06-11 13:48:25 +0100

    New search page

--- lib/reviews/features/search.dart
+++ lib/reviews/features/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vegan_reviews/reviews/reviews.dart';
+import 'package:vegan_reviews/shared/shared.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -8,9 +9,21 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-  
-  final PageController controller = PageController();
+
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  String foodType;
+  bool limited;
+  int stars;
+
+  @override
+  void initState() {
+    super.initState();
+    final ReviewsState state = context.bloc<ReviewsBloc>().state;
+    foodType = state.filterConfiguration.foodType;
+    limited = state.filterConfiguration.limited;
+    stars = state.filterConfiguration.stars;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +35,80 @@ class _SearchState extends State<Search> {
       ),
       body: BlocBuilder<ReviewsBloc, ReviewsState>(
         builder: (BuildContext builderContext, ReviewsState state) {
-          return GridView.count(
-            crossAxisCount: 2,
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              
+              Expanded(
+                child: GridView.count(
+                  padding: const EdgeInsets.all(5),
+                  crossAxisCount: 2,
+                  children: [
+                    _getFoodTypeSelector(),
+                    _getLimitedTypeSelector(),
+                    _getStarAmountSelector()
+                  ],
+                ),
+              ),
+              Button(
+                buttonText: 'Search',
+                onPressed: () {
+
+                },
+              )
             ],
           );
         },
       )
+    );
+  }
+
+  Widget _getFoodTypeSelector() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: FoodTypeSelector(
+            selected: (String value) {
+              setState(() {
+                foodType = value;
+              });
+            },
+            value: foodType,
+            validator: null,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getLimitedTypeSelector() {
+    return Card(
+      child: Center(
+        child: SwitchListTile(
+          title: const Text("Only show limited items"),
+          value: limited,
+          onChanged: (bool v) {
+            setState(() {
+              limited = v;
+            });
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _getStarAmountSelector() {
+    return Card(
+      child: Center(
+        child: StarAmountChooser(
+          value: stars,
+          selected: (int choice) {
+            setState(() {
+              stars = choice;
+            });
+          },
+        ),
+      ),
     );
   }
 }
