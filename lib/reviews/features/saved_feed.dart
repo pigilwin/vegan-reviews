92a1c9ca7565404322b06efa11b18282b6d4641c
Tim Remnant <timwebdesign2013@gmail.com>
2021-02-19 23:59:43 +0000

    Dont check for auth in saved feed

--- lib/reviews/features/saved_feed.dart
+++ lib/reviews/features/saved_feed.dart
@@ -40,16 +40,13 @@ class _SavedFeedState extends State<SavedFeed> with AutomaticKeepAliveClientMixi
                 }
               },
               toggleSave: () {
-                if (authenticationBloc.state is Authenticated) {
-
-                  ///
-                  /// The item is currently saved, removing
-                  ///
-                  if (state.savedReviews.contains(review.id)) {
-                    reviewsBloc.add(UnSaveReviewEvent(review));
-                  } else {
-                    reviewsBloc.add(SaveReviewEvent(review));
-                  }
+                ///
+                /// The item is currently saved, removing
+                ///
+                if (state.savedReviews.contains(review.id)) {
+                  reviewsBloc.add(UnSaveReviewEvent(review));
+                } else {
+                  reviewsBloc.add(SaveReviewEvent(review));
                 }
               }
             );
